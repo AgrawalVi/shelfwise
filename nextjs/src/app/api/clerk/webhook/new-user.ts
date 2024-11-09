aa48aff53b63234191f626fdfafb24bea45f952b
Vishrut Agrawal <zvishrut@gmail.com>
2024-11-09 15:22:27 -0600

    initialized clerk

---
+++ nextjs/src/app/api/clerk/webhook/new-user.ts
@@ -0,0 +1,14 @@
+import { clerkClient, UserJSON } from '@clerk/nextjs/server'
+import { db } from '@/lib/db'
+import { createUser } from '@/data/users'
+
+export const newUser = async (user: UserJSON) => {
+  // need to create the user in the database
+  const userEmail = user.email_addresses.filter(
+    (email) => email.id === user.primary_email_address_id,
+  )
+
+  await createUser(userEmail[0].email_address, user.first_name, user.last_name, user.id)
+
+  return { success: 'user created' }
+}
