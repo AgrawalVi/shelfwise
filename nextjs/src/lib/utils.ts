478194cd28e383aecd31bc8d3a549d016938e3b3
Vishrut Agrawal <zvishrut@gmail.com>
2024-11-09 16:36:47 -0600

    added all shadcn components

--- nextjs/src/lib/utils.ts
+++ nextjs/src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { GroceryItem } from "@/types"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export function transformGroceryItems(items: GroceryItem[], userId: string) {
-  return items.map((item) => ({
-    name: item.name,
-    expiresAt: item.expirationDate,
-    userId: userId,
-    perishable: item.perishable,
-  }))
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
