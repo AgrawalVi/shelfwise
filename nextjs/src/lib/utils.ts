02539c82bd1b8e98bf299ed5f4cc4160d91004ac
Vishrut Agrawal <zvishrut@gmail.com>
2024-11-09 16:39:12 -0600

    added back function

--- nextjs/src/lib/utils.ts
+++ nextjs/src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { GroceryItem } from "@/types"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function transformGroceryItems(items: GroceryItem[], userId: string) {
+  return items.map((item) => ({
+    name: item.name,
+    expiresAt: item.expirationDate,
+    userId: userId,
+    perishable: item.perishable,
+  }))
 }
