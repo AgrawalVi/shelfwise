4e79238bdf467ac9d22cd9e61a03d7837416ab7b
Vishrut Agrawal <zvishrut@gmail.com>
2024-11-10 10:38:43 -0600

    reformatted code

--- nextjs/src/app/api/clerk/webhook/update-user.ts
+++ nextjs/src/app/api/clerk/webhook/update-user.ts
@@ -1,16 +1,21 @@
-import { updateUserByClerkId } from '@/data/users'
-import { db } from '@/lib/db'
-import { UserJSON } from '@clerk/nextjs/server'
+import { updateUserByClerkId } from "@/data/users"
+import { db } from "@/lib/db"
+import { UserJSON } from "@clerk/nextjs/server"
 
 export const updateUser = async (user: UserJSON) => {
   // check email
   const userEmail = user.email_addresses.filter(
-    (email) => email.id === user.primary_email_address_id,
+    (email) => email.id === user.primary_email_address_id
   )
 
-  await updateUserByClerkId(user.id, userEmail[0].email_address, user.first_name, user.last_name)
+  await updateUserByClerkId(
+    user.id,
+    userEmail[0].email_address,
+    user.first_name,
+    user.last_name
+  )
 
   return {
-    success: 'user updated successfully',
+    success: "user updated successfully",
   }
 }
